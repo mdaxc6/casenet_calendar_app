3227206ac70fca925aa8743f7f4ecf2dc5d8bfa9
mdaxc6 <mikeabsher1128@gmail.com>
2021-09-21 22:14:24 -0500

    added all 46 districts

---
+++ static/js/checkbox_hiding.js
@@ -0,0 +1,204 @@
+// SHOW/HIDE COUNTIES UNTIL DISTRICT IS CHOSEN
+document.getElementById('CT01').onchange = function () {
+    var cart = document.getElementsByClassName('CT01_counties')[0];
+    if (this.checked) cart.classList.remove('hide');
+    else cart.classList.add('hide');
+}
+
+document.getElementById('CT02').onchange = function () {
+    var cart = document.getElementsByClassName('CT02_counties')[0];
+    if (this.checked) cart.classList.remove('hide');
+    else cart.classList.add('hide');
+}
+
+document.getElementById('CT03').onchange = function () {
+    var cart = document.getElementsByClassName('CT03_counties')[0];
+    if (this.checked) cart.classList.remove('hide');
+    else cart.classList.add('hide');
+}
+
+document.getElementById('CT04').onchange = function () {
+    var cart = document.getElementsByClassName('CT04_counties')[0];
+    if (this.checked) cart.classList.remove('hide');
+    else cart.classList.add('hide');
+}
+
+document.getElementById('CT05').onchange = function () {
+    var cart = document.getElementsByClassName('CT05_counties')[0];
+    if (this.checked) cart.classList.remove('hide');
+    else cart.classList.add('hide');
+}
+
+document.getElementById('CT08').onchange = function () {
+    var cart = document.getElementsByClassName('CT08_counties')[0];
+    if (this.checked) cart.classList.remove('hide');
+    else cart.classList.add('hide');
+}
+
+document.getElementById('CT09').onchange = function () {
+    var cart = document.getElementsByClassName('CT09_counties')[0];
+    if (this.checked) cart.classList.remove('hide');
+    else cart.classList.add('hide');
+}
+
+document.getElementById('CT10').onchange = function () {
+    var cart = document.getElementsByClassName('CT10_counties')[0];
+    if (this.checked) cart.classList.remove('hide');
+    else cart.classList.add('hide');
+}
+
+document.getElementById('CT12').onchange = function () {
+    var cart = document.getElementsByClassName('CT12_counties')[0];
+    if (this.checked) cart.classList.remove('hide');
+    else cart.classList.add('hide');
+}
+
+document.getElementById('CT13').onchange = function () {
+    var cart = document.getElementsByClassName('CT13_counties')[0];
+    if (this.checked) cart.classList.remove('hide');
+    else cart.classList.add('hide');
+}
+
+document.getElementById('CT14').onchange = function () {
+    var cart = document.getElementsByClassName('CT14_counties')[0];
+    if (this.checked) cart.classList.remove('hide');
+    else cart.classList.add('hide');
+}
+
+document.getElementById('CT15').onchange = function () {
+    var cart = document.getElementsByClassName('CT15_counties')[0];
+    if (this.checked) cart.classList.remove('hide');
+    else cart.classList.add('hide');
+}
+
+document.getElementById('CT17').onchange = function () {
+    var cart = document.getElementsByClassName('CT17_counties')[0];
+    if (this.checked) cart.classList.remove('hide');
+    else cart.classList.add('hide');
+}
+
+document.getElementById('CT18').onchange = function () {
+    var cart = document.getElementsByClassName('CT18_counties')[0];
+    if (this.checked) cart.classList.remove('hide');
+    else cart.classList.add('hide');
+}
+
+document.getElementById('CT20').onchange = function () {
+    var cart = document.getElementsByClassName('CT20_counties')[0];
+    if (this.checked) cart.classList.remove('hide');
+    else cart.classList.add('hide');
+}
+
+document.getElementById('CT24').onchange = function () {
+    var cart = document.getElementsByClassName('CT24_counties')[0];
+    if (this.checked) cart.classList.remove('hide');
+    else cart.classList.add('hide');
+}
+
+document.getElementById('CT25').onchange = function () {
+    var cart = document.getElementsByClassName('CT25_counties')[0];
+    if (this.checked) cart.classList.remove('hide');
+    else cart.classList.add('hide');
+}
+
+document.getElementById('CT26').onchange = function () {
+    var cart = document.getElementsByClassName('CT26_counties')[0];
+    if (this.checked) cart.classList.remove('hide');
+    else cart.classList.add('hide');
+}
+
+document.getElementById('CT27').onchange = function () {
+    var cart = document.getElementsByClassName('CT27_counties')[0];
+    if (this.checked) cart.classList.remove('hide');
+    else cart.classList.add('hide');
+}
+
+document.getElementById('CT28').onchange = function () {
+    var cart = document.getElementsByClassName('CT28_counties')[0];
+    if (this.checked) cart.classList.remove('hide');
+    else cart.classList.add('hide');
+}
+
+document.getElementById('CT30').onchange = function () {
+    var cart = document.getElementsByClassName('CT30_counties')[0];
+    if (this.checked) cart.classList.remove('hide');
+    else cart.classList.add('hide');
+}
+
+document.getElementById('CT32').onchange = function () {
+    var cart = document.getElementsByClassName('CT32_counties')[0];
+    if (this.checked) cart.classList.remove('hide');
+    else cart.classList.add('hide');
+}
+
+document.getElementById('CT33').onchange = function () {
+    var cart = document.getElementsByClassName('CT33_counties')[0];
+    if (this.checked) cart.classList.remove('hide');
+    else cart.classList.add('hide');
+}
+
+document.getElementById('CT34').onchange = function () {
+    var cart = document.getElementsByClassName('CT34_counties')[0];
+    if (this.checked) cart.classList.remove('hide');
+    else cart.classList.add('hide');
+}
+
+document.getElementById('CT35').onchange = function () {
+    var cart = document.getElementsByClassName('CT35_counties')[0];
+    if (this.checked) cart.classList.remove('hide');
+    else cart.classList.add('hide');
+}
+
+document.getElementById('CT36').onchange = function () {
+    var cart = document.getElementsByClassName('CT36_counties')[0];
+    if (this.checked) cart.classList.remove('hide');
+    else cart.classList.add('hide');
+}
+
+document.getElementById('CT37').onchange = function () {
+    var cart = document.getElementsByClassName('CT37_counties')[0];
+    if (this.checked) cart.classList.remove('hide');
+    else cart.classList.add('hide');
+}
+
+document.getElementById('CT39').onchange = function () {
+    var cart = document.getElementsByClassName('CT39_counties')[0];
+    if (this.checked) cart.classList.remove('hide');
+    else cart.classList.add('hide');
+}
+
+document.getElementById('CT40').onchange = function () {
+    var cart = document.getElementsByClassName('CT40_counties')[0];
+    if (this.checked) cart.classList.remove('hide');
+    else cart.classList.add('hide');
+}
+
+document.getElementById('CT41').onchange = function () {
+    var cart = document.getElementsByClassName('CT41_counties')[0];
+    if (this.checked) cart.classList.remove('hide');
+    else cart.classList.add('hide');
+}
+
+document.getElementById('CT42').onchange = function () {
+    var cart = document.getElementsByClassName('CT42_counties')[0];
+    if (this.checked) cart.classList.remove('hide');
+    else cart.classList.add('hide');
+}
+
+document.getElementById('CT43').onchange = function () {
+    var cart = document.getElementsByClassName('CT43_counties')[0];
+    if (this.checked) cart.classList.remove('hide');
+    else cart.classList.add('hide');
+}
+
+document.getElementById('CT44').onchange = function () {
+    var cart = document.getElementsByClassName('CT44_counties')[0];
+    if (this.checked) cart.classList.remove('hide');
+    else cart.classList.add('hide');
+}
+
+document.getElementById('CT45').onchange = function () {
+    var cart = document.getElementsByClassName('CT45_counties')[0];
+    if (this.checked) cart.classList.remove('hide');
+    else cart.classList.add('hide');
+}
--- static/js/index.js
+++ static/js/index.js
@@ -1,20 +1,3 @@
-// SHOW/HIDE COUNTIES UNTIL DISTRICT IS CHOSEN
-document.getElementById('CT13').onchange = function () {
-    var cart = document.getElementsByClassName('CT13_counties')[0];
-    if (this.checked) cart.classList.remove('hide');
-    else cart.classList.add('hide');
-}
-document.getElementById('CT20').onchange = function () {
-    var cart = document.getElementsByClassName('CT20_counties')[0];
-    if (this.checked) cart.classList.remove('hide');
-    else cart.classList.add('hide');
-}
-document.getElementById('CT45').onchange = function () {
-    var cart = document.getElementsByClassName('CT45_counties')[0];
-    if (this.checked) cart.classList.remove('hide');
-    else cart.classList.add('hide');
-}
-
 // SUBMISSION HANDLING
 function handleSubmit(event) {
     event.preventDefault();
@@ -111,3 +94,5 @@ function loading(){
 
 const form = document.querySelector('form');
 form.addEventListener('submit', handleSubmit);
+
+
