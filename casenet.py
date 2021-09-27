82d41efefbf8f0d1c029b164d7355b22934a9d2e
mdaxc6 <mikeabsher1128@gmail.com>
2021-09-27 17:21:02 -0500

    added flask files

--- casenet.py
+++ casenet.py
@@ -10,6 +10,7 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 
+
 # CHROME DRIVER
 chromedriver_location = "chrome_driver/chromedriver"
 
@@ -53,7 +54,7 @@ def authenticate():
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
+                os.environ['credentials.json'], SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
         with open('token.json', 'w') as token:
